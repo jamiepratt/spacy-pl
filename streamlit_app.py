b75451040c3ae1f8e378c5a924ac9ef3e09e8ba9
James Pratt <me@jamiep.org>
2023-12-18 10:28:27 +0100

    need a virtual environment since get a file permission error when
    trying to download language model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 # streamlit_app.py
 import spacy_streamlit
 import spacy.cli
+import spacy.util
 
 models = ["en_core_web_sm", "en_core_web_md"]
 default_text = "Sundar Pichai is the CEO of Google."
+
+# Create a virtual environment
+venv_path = "/app/.venv"
+spacy.util.set_env("VIRTUAL_ENV", venv_path)
 
 # Download the language model if not already installed
 for model in models:
