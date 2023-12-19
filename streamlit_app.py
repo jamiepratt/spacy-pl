f64eb413d622ae4e2a0d634204d18788409b9ab0
James Pratt <me@jamiep.org>
2023-12-19 11:28:04 +0100

    removing unnecessary models
    and import statments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 # streamlit_app.py
 import spacy_streamlit
-import en_core_web_sm, en_core_web_md, en_core_web_lg, en_core_web_trf
 
-models = ["pl_core_news_sm", "pl_core_news_md", "pl_core_news_lg",
-            "en_core_web_sm", "en_core_web_md", "en_core_web_lg", "en_core_web_trf"]
+models = ["pl_core_news_sm", "pl_core_news_md"
+            "en_core_web_sm", "en_core_web_md"]
 default_text = "Marta poszła spać. Była jedenasta wieczorem. Ona wyłączyła światło i położyła się do łóżka. W pokoju było ciemno i cicho. Marta próbowała usnąć, ale nie mogła."
 
 spacy_streamlit.visualize(models, default_text)
