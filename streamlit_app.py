eb2c4edb29e6b9c193fd514791df51d7b4f7a7ca
James Pratt <me@jamiep.org>
2023-12-20 08:57:54 +0100

    fix for model select box

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # streamlit_app.py
 import spacy_streamlit
 
-models = ["pl_core_news_sm", "pl_core_news_md"
+models = ["pl_core_news_sm", "pl_core_news_md",
             "en_core_web_sm", "en_core_web_md"]
 default_text = "Marta poszła spać. Była jedenasta wieczorem. Ona wyłączyła światło i położyła się do łóżka. W pokoju było ciemno i cicho. Marta próbowała usnąć, ale nie mogła."
 
