f9318db6f6e7ebe332bbe9a14aaf8d085a584e79
James Pratt <me@jamiep.org>
2023-12-21 05:44:38 +0100

    install all 26 of the latest language models

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,33 @@
 # streamlit_app.py
 import spacy_streamlit
+models = [
+    "pl_core_news_sm",
+    "ca_core_news_sm",
+    "da_core_news_sm",
+    "de_core_news_sm",
+    "el_core_news_sm",
+    "en_core_web_sm",
+    "es_core_news_sm",
+    "fi_core_news_sm",
+    "fr_core_news_sm",
+    "hr_core_news_sm",
+    "it_core_news_sm",
+    "ja_core_news_sm",
+    "ko_core_news_sm",
+    "lt_core_news_sm",
+    "mk_core_news_sm",
+    "nb_core_news_sm",
+    "nl_core_news_sm",
+    "pt_core_news_sm",
+    "ro_core_news_sm",
+    "sl_core_news_sm",
+    "sv_core_news_sm",
+    "ru_core_news_sm",
+    "uk_core_news_sm",
+    "xx_ent_wiki_sm",
+    "xx_sent_ud_sm",
+    "zh_core_web_sm"]
 
-models = ["pl_core_news_sm", "pl_core_news_md",
-            "en_core_web_sm", "en_core_web_md"]
 default_text = "Marta poszła spać. Była jedenasta wieczorem. Ona wyłączyła światło i położyła się do łóżka. W pokoju było ciemno i cicho. Marta próbowała usnąć, ale nie mogła."
 
 spacy_streamlit.visualize(models, default_text)
